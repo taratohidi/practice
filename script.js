0030f28a738fbf59fedf45ebadeffea2176ec289
Tara Tohidi <tara.tohidi@gmail.com>
2022-08-16 12:51:51 -0700

    Practice scope

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName} You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName = 'Tina';
+      const str = `oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      // const output = 'new output';
+
+      output = 'new output';
+    }
+
+    console.log(millenial);
+    console.log(output);
+  }
+
+  printAge();
+  return age;
+}
+
+const firstName = 'TARA';
+calcAge(1984);

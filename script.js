1647d65a9940d2eb398712051b1246215344a4c2
Tara Tohidi <tara.tohidi@gmail.com>
2022-08-17 10:12:27 -0700

    Practice

--- script.js
+++ script.js
@@ -91,6 +91,22 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -98,43 +114,62 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+  // ES6 enhanced object literals
+  openingHours,
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  order: function (startIndex, mainIndex) {
-    return [this.starterMenu[startIndex], this.mainMenu[mainIndex]];
-  },
-
-  orderDelivery: function ({ startIndex, mainIndex, time, address }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(
-      `Order recived! ${this.starterMenu[startIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
   },
 };
 
-////// destructuring array
+//////OPTINAL CHAINING
+// if restaurant.openingHours.mon console.log(restaurant.openingHours.mon.open)
+// console.log(restaurant.openingHours.mon?.open);
+// console.log(restaurant.openingHours?.mon?.open);
+// console.log(restaurant.openingHours.fri?.open);
+
+// for (const day of weekdays) {
+//   const open = restaurant.openingHours[day]?.open ?? 'CLOSED';
+//   console.log(open);
+// }
+// console.log(restaurant.order?.(0, 1) ?? 'method is not exist.');
+
+/////// LOOP OVER OBJECT
+for (const day of Object.keys(openingHours)) {
+  console.log(day);
+}
+
+////// FOR-OF LOOP
+// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// for (const item of menu) console.log(item);
+// for (const item of menu.entries()) console.log(item);
+// for (const item of menu.entries()) console.log(`${item[0] + 1} : ${item[1]}`);
+// for (const [i, el] of menu.entries()) console.log(`${i + 1} : ${el}`);
+
+////// DESTRUCTURING ARRAYS AND OBJECTS
 // let [main, secondary] = restaurant.categories;
 // console.log(main, secondary);
-
 // [main, secondary] = [secondary, main];
 // console.log(main, secondary);
-
 // const [starter, mainCourse] = restaurant.order(2, 0);
 // console.log(starter, mainCourse);
-
-////// Destructuring Object
 
 // const { name, openingHours, categories } = restaurant;
 // console.log(name, openingHours, categories);
@@ -142,7 +177,7 @@ const restaurant = {
 // const { name: restaurantName, menu = [] } = restaurant;
 // console.log(restaurantName, menu);
 
-///// nested object
+///// NESTED OBJECT
 // const {
 //   fri: { open, close },
 //   sat: { open: o, close: c },
@@ -156,15 +191,14 @@ const restaurant = {
 //   startIndex: 0,
 // });
 
-////The Spread Operator
-
+////THE SPREAD OPERATOR
 // const newMenu = [...restaurant.mainMenu, 'Gnocci'];
 // console.log(newMenu);
 
-// // Shallow Copy
+//// Shallow Copy
 // const menuCopy = [...restaurant.mainMenu];
 
-// // Join 2 array
+///// Join 2 array
 // const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
 // console.log(menu);
 
@@ -262,27 +296,51 @@ const game = {
   },
 };
 
-const [player1, player2] = game.players;
-console.log(player1, player2);
+// const [player1, player2] = game.players;
+// console.log(player1, player2);
 
-const [gk, ...fieldPlayers] = player1;
-console.log(gk, fieldPlayers);
+// const [gk, ...fieldPlayers] = player1;
+// console.log(gk, fieldPlayers);
 
-const allPlayers = [...player1, ...player2];
-console.log(allPlayers);
+// const allPlayers = [...player1, ...player2];
+// console.log(allPlayers);
 
-const players1Final = ['Thiago', 'Coutinho', 'Perisic', ...player1];
-console.log(players1Final);
+// const players1Final = ['Thiago', 'Coutinho', 'Perisic', ...player1];
+// console.log(players1Final);
 
-const { team1, x: draw, team2 } = game.odds;
-console.log(team1, draw, team2);
+// const { team1, x: draw, team2 } = game.odds;
+// console.log(team1, draw, team2);
 
-const printGoals = function (...players) {
-  console.log(players);
-  console.log(players.length);
-};
+// const printGoals = function (...players) {
+//   console.log(players);
+//   console.log(players.length);
+// };
 
-printGoals(...game.scored);
+// printGoals(...game.scored);
 
-(team1 > team2 && console.log('team1')) ||
-  (team1 < team2 && console.log('team2'));
+// (team1 > team2 && console.log('team1')) ||
+//   (team1 < team2 && console.log('team2'));
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/

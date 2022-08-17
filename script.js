87ad0d8dd69537dbb91d0d09e25e26d7bf33a6eb
Tara Tohidi <tara.tohidi@gmail.com>
2022-08-17 14:20:52 -0700

    Challange #4

--- script.js
+++ script.js
@@ -406,16 +406,81 @@ const gameEvents = new Map([
   [92, '🔶 Yellow card'],
 ]);
 
-const events = [...gameEvents.values()];
-console.log(events);
+// const events = [...gameEvents.values()];
+// console.log(events);
 
-gameEvents.delete(64);
-console.log(gameEvents);
+// gameEvents.delete(64);
+// console.log(gameEvents);
 
-const eventEntries = [...gameEvents.entries()];
-console.log(eventEntries);
+// const eventEntries = [...gameEvents.entries()];
+// console.log(eventEntries);
 
-for (const [key, value] of eventEntries) {
-  (key < 45 && console.log(`[FIRST HALF] ${key}: ${value}`)) ||
-    (key > 45 && console.log(`[SECOND HALF] ${key}: ${value}`));
-}
+// for (const [key, value] of eventEntries) {
+//   (key < 45 && console.log(`[FIRST HALF] ${key}: ${value}`)) ||
+//     (key > 45 && console.log(`[SECOND HALF] ${key}: ${value}`));
+// }
+
+// function capitalizeName(name) {
+//   const nameArr = name.split(' ');
+//   let newNameArr = [];
+//   for (const name of nameArr) {
+//     // newNameArr.push(name[0].toUpperCase() + name.slice(1));
+//     newNameArr.push(name.replace(name[0], name[0].toUpperCase()));
+//   }
+
+//   const newName = newNameArr.join(' ');
+
+//   return newName;
+// }
+// console.log(capitalizeName('tara tohidi'));
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const row of rows) {
+    const newRow = row.toLowerCase().trim().split('_');
+    const [first, second] = newRow;
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(rows.indexOf(row) + 1)}`);
+  }
+});

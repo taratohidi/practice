e6ad4ea320152ee4db6f7deab07c84c15d6c4747
Tara Tohidi <tara.tohidi@gmail.com>
2022-08-17 12:17:06 -0700

    Challange #3

--- script.js
+++ script.js
@@ -237,6 +237,8 @@ const restaurant = {
 // console.log(res1);
 // console.log(res2.owner);
 
+// console.log(new Set('tara'));
+
 // Challenge #1
 /* 
 We're building a football betting app (soccer for my American friends 😅)!
@@ -344,20 +346,20 @@ BONUS: Create an object called 'scorers' which contains the names of the players
 
 GOOD LUCK 😀
 */
-const { scored, odds } = game;
-const oddsEntries = Object.entries(odds);
+// const { scored, odds } = game;
+// const oddsEntries = Object.entries(odds);
 
-for (const [goal, player] of scored.entries()) {
-  console.log(`Goal ${goal + 1}: ${player}`);
-}
+// for (const [goal, player] of scored.entries()) {
+//   console.log(`Goal ${goal + 1}: ${player}`);
+// }
 
-const oddsValue = Object.values(odds);
-let avg = 0;
-for (const odd of oddsValue) {
-  avg += odd / oddsValue.length;
-}
-// avg /= oddsValue.length;
-console.log(avg);
+// const oddsValue = Object.values(odds);
+// let avg = 0;
+// for (const odd of oddsValue) {
+//   avg += odd / oddsValue.length;
+// }
+// // avg /= oddsValue.length;
+// console.log(avg);
 
 // const oddss = Object.values(game.odds);
 // let average = 0;
@@ -365,12 +367,55 @@ console.log(avg);
 // average /= oddss.length;
 // console.log(average);
 
-for (const [key, value] of oddsEntries) {
-  (game[key] && console.log(`Odd of victory ${game[key]}: ${value}`)) ||
-    (!game[key] && console.log(`Odd of draw: ${value}`));
+// for (const [key, value] of oddsEntries) {
+//   (game[key] && console.log(`Odd of victory ${game[key]}: ${value}`)) ||
+//     (!game[key] && console.log(`Odd of draw: ${value}`));
+// }
+// const scorers = {};
+// for (const player of game.scored) {
+//   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+// }
+// console.log(scorers);
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+const events = [...gameEvents.values()];
+console.log(events);
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+const eventEntries = [...gameEvents.entries()];
+console.log(eventEntries);
+
+for (const [key, value] of eventEntries) {
+  (key < 45 && console.log(`[FIRST HALF] ${key}: ${value}`)) ||
+    (key > 45 && console.log(`[SECOND HALF] ${key}: ${value}`));
 }
-const scorers = {};
-for (const player of game.scored) {
-  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
-}
-console.log(scorers);
